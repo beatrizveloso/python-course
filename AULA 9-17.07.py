17127b1013ae03c9bc521d90ee8660a07c9fbf05
beatrizveloso <beatrizoslov6@gmail.com>
2024-07-17 15:02:20 -0300

    feat: matrizes

--- AULA 9-17.07.py
+++ AULA 9-17.07.py
@@ -1,2 +1,39 @@
-# Matrizes
+# Aula de Matrizes
+# Importando biblioteca ramdômica
 
+import random
+
+# definir o tamanho da matriz
+TAM = 3
+
+# definindo o laço para a matriz que
+# inteira TAM vezes
+mat1 = [[] for _ in range(TAM)]
+mat2 = [[] for _ in range(TAM)]
+mat3 = [[0 for _ in range(TAM)] for _ in range(TAM)]
+
+# Função soma
+def soma():
+    for lin in range(TAM):
+        for col in range(TAM):
+            mat3[lin][col]= (mat1[lin][col] + mat2[lin][col])
+
+# Preenchendo a matriz com números aleatórios
+for lin in range(TAM):
+    for col in range(TAM):
+        mat1[lin].append(random.randrange(0,10))
+        mat2[lin].append(random.randrange(0,10))
+
+# para exibir
+print("Matriz 1: ")
+for cont in mat1:
+    print(cont)
+
+print("Matriz 2: ")
+for cont in mat2:
+    print(cont)
+
+print("Matriz Soma: ")
+soma()
+for cont in mat3:
+    print(cont)

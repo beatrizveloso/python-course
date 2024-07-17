7e32c5d71271522951b4cdb5b15e2f68e1a80cb2
beatrizveloso <beatrizoslov6@gmail.com>
2024-07-17 17:06:29 -0300

    feat: created matriz

--- AULA 9-17.07.py
+++ AULA 9-17.07.py
@@ -4,7 +4,7 @@
 import random
 
 # definir o tamanho da matriz
-TAM = 3
+TAM = 50
 
 # definindo o laço para a matriz que
 # inteira TAM vezes
@@ -17,6 +17,18 @@ def soma():
     for lin in range(TAM):
         for col in range(TAM):
             mat3[lin][col]= (mat1[lin][col] + mat2[lin][col])
+
+# Função subtração
+def sub():
+    for lin in range(TAM):
+        for col in range(TAM):
+            mat3[lin][col]= (mat1[lin][col] - mat2[lin][col])
+
+# Função multiplicação
+def mult():
+    for lin in range(TAM):
+        for col in range(TAM):
+            mat3[lin][col]= (mat1[lin][col] * mat2[lin][col])
 
 # Preenchendo a matriz com números aleatórios
 for lin in range(TAM):
@@ -33,7 +45,19 @@ print("Matriz 2: ")
 for cont in mat2:
     print(cont)
 
+resp = input("Escolha sua operação: ")
+
 print("Matriz Soma: ")
 soma()
+for cont in mat3:
+    print(cont)
+
+print("Matriz Subtração: ")
+sub()
+for cont in mat3:
+    print(cont)
+
+print("Matriz Multiplicação: ")
+mult()
 for cont in mat3:
     print(cont)
